f2f4b69b6c334570b91569c88978b2ed3f6eff3c
John Yusuf <johnnymoy2007@gmail.com>
2023-02-20 09:51:39 +0100

    last addition

---
+++ index.js
@@ -0,0 +1,14 @@
+function multiply(x, y) {
+    return x * y
+}
+console.log(multiply(5, 10))
+
+const firstname = "john";
+const lastname = "Yusuf";
+const track  = "Frontend";
+const skills = ["Html, CSS"];
+const fav_color = "sky blue";
+const Nigeria_year_of_independence = 1960;
+
+const Aboutjohn = "My name is " + firstname + ", and my lastname is " + lastname + ". I'm in " + track + " track. " + " My skills include " + skills + ". My favorite colors " + fav_color + ". Year of independent " + year_of_independence 
+console.log(Aboutjohn)
